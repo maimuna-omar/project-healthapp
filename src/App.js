0b4246ebf8272c92575789a10ceeb77c8646e03d
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-06-06 22:09:52 +0300

    Fetched data from JSON

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
+import Dashboard from './components/Dashboard';
 
 function App() {
+  const [userData, setUserData] = useState([]);
+  const baseUrl = 'https://my-json-server.typicode.com/Wambuiwambugu/Group-8-REST-API/users'
+
+  useEffect(() => {
+    fetch(`${baseUrl}`)
+    .then(res => res.json())
+    .then(data => setUserData(data))
+    
+  },[])
+  // console.log(userData)
+
+
   return (
     <div className="App">
-      welcome to App!
+      Welcome to App
     </div>
   );
 }
