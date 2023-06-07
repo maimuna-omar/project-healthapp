123b5b68e99bd4930724817b8b9319d9850e633c
lgmakena <linetmakenag@gmail.com>
2023-06-07 09:42:16 +0300

    app and index

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Landingpage from './Landingpage';
-import Header from './Header';
+import Landingpage from './Components/Landingpage/Landingpage';
+import Header from './Components/Landingpage/Header';
+
+
 
 function App() {
   return (
     <div >
        <Header/>
        <Landingpage/>
+      
     </div>
   );
 }
