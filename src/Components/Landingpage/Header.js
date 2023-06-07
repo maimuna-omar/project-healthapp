a60eb5a92361c97ac673a5fa9db34e645d0bbcbe
lgmakena <linetmakenag@gmail.com>
2023-06-07 20:08:18 +0300

    style green

--- src/Components/Landingpage/Header.js
+++ src/Components/Landingpage/Header.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom'
 
 function Header() {
   return (
+    
     <div className='header'>
         <Link to='/'>Dashboard</Link>
         <Link to='/healthgoals'>HealthGoals</Link>
         <Link to='/dailyactivity'>Daily Activity</Link>
     </div>
+    
   )
 }
 
